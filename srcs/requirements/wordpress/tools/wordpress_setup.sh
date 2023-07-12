9a1621d3211cc20250772c943f00b7952b966cf4
tamagoyakii <rlawlgus4063@naver.com>
2023-07-12 17:57:01 +0900

    [feat] conf 파일 추가하는 부분 구현

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -2,26 +2,12 @@
 
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	wp core download --path=/var/www/html --allow-root && \
-
-	wp config create --allow-root \
-	--dbname=$DB_NAME \
-	--dbuser=$DB_USER \
-	--dbpass=$DB_PWD \
-	--dbhost=$DB_HOST \
-	--dbcharset="utf8" && \
-
-	wp core install --allow-root \
-	--url=$DOMAIN_NAME \
-	--title=$WP_TITLE \
-	--admin_user=$WP_ADMIN_USER \
-	--admin_password=$WP_ADMIN_PWD \
-	--admin_email=$WP_ADMIN_EMAIL && \
-
-	wp user create --allow-root \
-	$WP_USER $WP_EMAIL --role=author \
-	--user_pass=$WP_PWD
-	
+	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOST --dbcharset="utf8" && \
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL && \
+	wp user create --allow-root $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD
 	wp plugin update --all --path=/var/www/html --allow-root
 fi
 
-# exec php-fpm7.4 -F
+chown -R www-data:www-data /var/www/
+
+exec php-fpm7.4 -F
