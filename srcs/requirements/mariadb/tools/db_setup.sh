2118166c60c47f25bd0ad5f835e42e5adbed69ea
tamagoyakii <rlawlgus4063@naver.com>
2023-07-25 21:17:57 +0900

    [feat] 주석 추가

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# mysql 데이터베이스가 처음 실행되는 경우에만 실행
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 	service mariadb start
 
@@ -10,7 +11,10 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
 	mysql -e "FLUSH PRIVILEGES;"
 
+	# 설정 적용을 위해 서버 종료
+	# root 사용자로 접속하여 스크립트가 종료되더라도 서버가 백그라운드로 실행되는 것을 방지
 	mysqladmin -uroot -p$DB_ROOT_PWD shutdown
 fi
 
+# mysqld 서버를 포그라운드에서 실행 
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# wordpress가 처음 설치될 때 실행
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	wp core download --path=/var/www/html --allow-root && \
 	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOST --dbcharset="utf8" && \
@@ -8,6 +9,8 @@ if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	wp plugin update --all --path=/var/www/html --allow-root
 fi
 
-chown -R www-data:www-data /var/www/
+# wordpress 파일 및 디렉토리 권한을 뒙 서버의 사용자와 그룹에 소유
+# chown -R www-data:www-data /var/www/
 
+# php-fpm을 포그라운드로 실행하여 웹 서버와 wordpress 동작
 exec php-fpm7.4 -F
