328c2f700f436faa58fe72b2385971ca90391ab1
tamagoyakii <rlawlgus4063@naver.com>
2023-07-25 19:27:50 +0900

    [fix] 스크립트 수정

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -1,28 +1,16 @@
 #!/bin/sh
 
-cat << EOF > config.sql
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PWD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
-USE $DB_NAME;
-FLUSH PRIVILEGES;
-EOF
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+	service mariadb start
 
-service mariadb start && sleep 2 && mysql -uroot -p$DB_ROOT_PWD< config.sql && mysqladmin -uroot -p$DB_ROOT_PWD shutdown && mysqld_safe
+	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+	mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PWD';"
+	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION;"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
+	mysql -e "FLUSH PRIVILEGES;"
 
-# service mariadb start
+	mysqladmin -uroot -p$DB_ROOT_PWD shutdown
+fi
 
-# if [ $? -ne 0 ]; then
-# 	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-# 	mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';"
-# 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PWD';"
-# 	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION;"
-# 	mysql -e "USE $DB_NAME;"
-# 	mysql -e "FLUSH PRIVILEGES;"
-# fi
-
-# service mariadb stop
-
-# exec mysqld_safe
+exec mysqld_safe
