e8b695c3e9563bfa13030ac986d659efcf5fea4c
tamagoyakii <rlawlgus4063@naver.com>
2023-07-26 15:26:07 +0900

    [fix] 실행 조건문 수정

--- srcs/requirements/mariadb/tools/db_setup.sh
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # mysql 데이터베이스가 처음 실행되는 경우에만 실행
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 	service mariadb start
 
 	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 	mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PWD';"
 	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION;"
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';"
 	mysql -e "FLUSH PRIVILEGES;"
@@ -15,6 +14,8 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
 	# root 사용자로 접속하여 스크립트가 종료되더라도 서버가 백그라운드로 실행되는 것을 방지
 	mysqladmin -uroot -p$DB_ROOT_PWD shutdown
 fi
+
+chown -R mysql:mysql /run/mysqld
 
 # mysqld 서버를 포그라운드에서 실행 
 exec mysqld_safe
